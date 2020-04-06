2c5b7e7ad7eea23ac49380620bf11d648b988be2
Anthea Harvey-Brown <tharveybrown@gmail.com>
2020-04-06 19:23:57 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -8,11 +8,12 @@ end
 
 def pioneer
   # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
-  { :name => "Grace Hopper" }
+  { name: "Grace Hopper" }
 end
 
 
 def id_hash_generator(number)
   # return a hash with a key :id assigned to the provided number
+  { id: number }
   
 end
